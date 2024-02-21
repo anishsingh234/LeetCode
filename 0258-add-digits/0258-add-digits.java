06013335095cf7888379480f52f28e04643ae163
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2024-02-21 17:16:13 +0530

    Time: 1 ms (52.62%), Space: 40.6 MB (77.01%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum=0;
+        int sum2=0;
+        if(num<=9){
+            return num;
+        }
+        while(num>0){
+            sum=sum+num%10;
+            num=num/10;
+        }
+        if (sum>9){
+              return addDigits(sum);
+        }
+        else{
+            return sum;
+        }
+    }
+}
