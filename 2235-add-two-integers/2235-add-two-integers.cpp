12a32f1d363b7933103ceb2329ab8efbfa24aafa
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2024-03-08 09:07:10 +0530

    Time: 3 ms (37.3%), Space: 7.1 MB (73.59%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int sum(int num1, int num2) {
-         while (num2 != 0) {
-            int sumWithoutCarry = num1 ^ num2;
-            int carry = (num1 & num2) << 1;
-            num1 = sumWithoutCarry;
-            num2 = carry;
-        }
-        return num1; 
+        return num1 + num2; 
         
     }
 };
