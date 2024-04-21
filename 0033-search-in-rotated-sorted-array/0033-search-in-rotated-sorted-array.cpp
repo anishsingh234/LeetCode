4c92dc4d7d2d116fe7907840bb80826d65bbb980
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2024-04-21 18:44:06 +0530

    Time: 2 ms (65.26%), Space: 13.5 MB (35.25%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,24 +5,19 @@ public:
         int end = nums.size() - 1;
         
         while (start <= end) {
-            int mid = start + (end - start) / 2; // To avoid integer overflow
-            
+            int mid = start + (end - start) / 2; 
             if (nums[mid] == target) {
                 return mid;
             }
             
-            // Check if the left half is sorted
             if (nums[start] <= nums[mid]) {
-                // Check if the target is in the left half
                 if (target >= nums[start] && target < nums[mid]) {
                     end = mid - 1;
                 } else {
                     start = mid + 1;
                 }
             } 
-            // If the left half is not sorted, then the right half must be sorted
             else {
-                // Check if the target is in the right half
                 if (target > nums[mid] && target <= nums[end]) {
                     start = mid + 1;
                 } else {
@@ -30,7 +25,6 @@ public:
                 }
             }
         }
-        
         return -1;
     }
 };
