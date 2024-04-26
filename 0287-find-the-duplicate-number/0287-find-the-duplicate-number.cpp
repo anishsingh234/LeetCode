888e0d186b37527835193ec6d52c87db212ce5ac
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2024-04-26 21:36:30 +0530

    Time: 69 ms (87.92%), Space: 67.8 MB (20.28%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -6,7 +6,6 @@ public:
         for(int i=0; i<n; i++){
             arr1[nums[i]]++;
         }
-        int a=INT_MIN;
         for(int i=0; i<(1000001); i++){
            if(arr1[i]>1){
             return i;
