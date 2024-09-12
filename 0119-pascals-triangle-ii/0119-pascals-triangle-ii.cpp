eed9c583b7bb37a27ba8587dd5b7f612ecd18777
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2024-09-12 07:17:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (55.3%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int n) {
+        long long ans=1;
+        vector<int> arr;
+        arr.push_back(ans);
+        for(int i=1; i<=n; i++){
+            ans=ans*(n-i+1);
+            ans=ans/i;
+            arr.push_back(ans);
+        }
+        return arr;
+        
+    }
+};
