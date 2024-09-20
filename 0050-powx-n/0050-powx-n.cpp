0ad10cdfa6a3897aa5167ff2a1bf2d10e2cf7a78
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2024-09-20 19:28:03 +0530

    Time: 3 ms (38.91%), Space: 8.4 MB (43.78%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long nl=n;
-        bool IsNegative=false;
-        double ans = 1;
-        if(n<0){
-            IsNegative = true;
-            nl=nl*-1;
+       double ans=1;
+       long long p=n;
+        bool isNegative=false;
+       if(p<0){
+           p=-p;
+           x=1/x;
+       }
+       while(p>0){
+        if(p&1){
+            ans=ans*x;
         }
-        while(nl>0){
-            if(nl%2==1){
-                ans=ans*x;
-            }
-            x=x*x;
-            nl=nl/2;
-        }
-        if(IsNegative){
-            return (1/ans);
-        }
-        return ans;
+        x=x*x;
+        p=p>>1;
+       }
+       return ans ;
     }
 
 };
