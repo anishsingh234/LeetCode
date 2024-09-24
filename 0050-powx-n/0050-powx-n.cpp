e721d9f75c4f0d846a62fd265e39b25bfb992a79
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2024-09-24 18:31:11 +0530

    Time: 0 ms (100%), Space: 8.2 MB (86.89%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,14 +3,14 @@ public:
     double myPow(double x, int n) {
        double ans=1;
        long long p=n;
-        bool isNegative=false;
        if(p<0){
-           p=-p;
-           x=1/x;
+        p=-p;
+        x=1/x;
        }
        while(p>0){
         if(p&1){
             ans=ans*x;
+            p--;
         }
         x=x*x;
         p=p>>1;
