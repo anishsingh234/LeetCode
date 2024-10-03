db64c06909c02bbae0d2f64ac89547603c6e578a
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2024-10-03 19:41:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int maximizeSum(vector<int>& nums, int k) {
       int n=nums.size()-1;
-      sort(nums.begin(),nums.end());
       int ans=0;
       for(int i=0; i<k; i++){
         ans=ans+nums[n];
