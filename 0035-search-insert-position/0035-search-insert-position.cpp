2925eeb1a3804c6c90977d05f491f1acb0186619
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2024-10-10 19:30:54 +0530

    Time: 4 ms (36.47%), Space: 12.4 MB (62.34%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int i=0;
-        int c;
-        if(target==0 && nums[0]>0){
-            return 0;
-        }
-        if(target<nums[0]){
-            return 0;
-        }
-        while(i<nums.size() && nums[i]<=target){
-            if(nums[i]==target){
-                return i;
+        int n=nums.size();
+        int ans=n;
+        int start=0;
+        int end=n-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                end=mid-1;
             }
-            c=i;
-            i++;
+            else{
+                start=mid+1;
+            }
         }
-        return c+1;  
+        return ans;
     }
 };
