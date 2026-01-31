e18c8ed8f3d62afe3cadaa1c257a447780a5fbc9
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2026-01-31 19:43:46 +0530

    Time: 0 ms (100%), Space: 9.9 MB (10.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,16 +3,17 @@ public:
     bool isAnagram(string s, string t) {
         int freqTable[256]={0};
         for(int i=0; i<s.size(); i++){
-            freqTable[s[i]]++;   
+            freqTable[s[i]]++;
         }
+
         for(int i=0; i<t.size(); i++){
-            freqTable[t[i]]--; 
+            freqTable[t[i]]--;
         }
         for(int i=0; i<256; i++){
             if(freqTable[i]!=0){
                 return false;
             }
         }
-        return true;  
+        return true;
     }
 };
