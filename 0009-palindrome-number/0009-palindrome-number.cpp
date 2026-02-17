89a7b95340e7a6e1c7a5731a29182d42ca6b67e4
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2026-02-17 19:19:35 +0530

    Time: 0 ms (100%), Space: 8.5 MB (91.37%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int temp=x;
+        long long int sum=0;
+        while(x>0){
+            int rem=x%10;
+            sum=sum*10+rem;
+            x=x/10;
+        }
+        if(sum==temp){
+            return true;
+        }
+        return false;
+    }
+};
