b8b7fb9de005ddc108e3270c4d8686b6baf01cea
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2026-02-17 19:13:35 +0530

    Time: 42 ms (6.2%), Space: 8.2 MB (9.54%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int sum(int i,int n){
-        int sum=0; 
+    int sum(int i ,int n){
+        int result=0;
         while(i<=n){
-            sum+=i;
+            result+=i;
             i++;
         }
-        return sum;
+        return result;
     }
     int pivotInteger(int n) {
         int i=0;
         while(i<=n){
             int ab=sum(0,i);
             int bc=sum(i,n);
+
             if(ab==bc){
                 return i;
             }
             i++;
         }
-        return -1; 
+        return -1;
     }
 };
