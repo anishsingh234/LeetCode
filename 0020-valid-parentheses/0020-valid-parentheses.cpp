f03b13839162b42a3216f2a600f6a3bd1a4ebfca
anish <129846065+anishsingh234@users.noreply.github.com>
2026-03-25 13:08:53 +0530

    Time: 0 ms (100%), Space: 8.8 MB (87.93%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;  // Stack to store opening brackets
-
-        for (auto it : s) {
-            if (it == '(' || it == '{' || it == '[')
-                st.push(it);  // Push opening brackets to stack
-            else {
-                if (st.empty()) return false;  // No matching opening bracket
-                char ch = st.top();
+        stack <char>st;
+        for(auto it:s){
+            if(it == '(' || it == '{' || it =='['){
+                st.push(it);
+            }
+            else{
+                if(st.empty()) return false;
+                char ch=st.top();
                 st.pop();
 
-                // Check for matching pair
-                if ((it == ')' && ch == '(') ||
-                    (it == ']' && ch == '[') ||
-                    (it == '}' && ch == '{'))
+                if((it == ')' && ch == '(') || (it == ']' &&  ch=='[') || (it== '}' && ch =='{')){
                     continue;
-                else
+                }
+                else{
                     return false;
+                }
             }
         }
-        return st.empty();  // True if all brackets matched
+        return st.empty();
     }
 };
